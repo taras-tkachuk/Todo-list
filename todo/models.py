bfbe28bde6a2a4b2089721057d54a1cbe2762481
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-08-31 14:59:24 +0300

    Add models to admin panel

--- todo/models.py
+++ todo/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=69)
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     content = models.TextField()
@@ -11,3 +14,6 @@ class Task(models.Model):
     deadline = models.DateTimeField(null=True, blank=True)
     is_done = models.BooleanField()
     tags = models.ManyToManyField(Tag, related_name="tasks", blank=True)
+
+    def __str__(self):
+        return f"{self.content} - {self.is_done}"
