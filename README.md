1cb23c86b4694fb734744981a3c852aacadd25e5
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-09-01 14:03:39 +0300

    Update readme.md and add secret key to .env

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# Todo-list" 
+# Todo-list
+
+Todo List is a web application that allows users to organize their tasks and manage them efficiently. The application enables users to create, view, update, and delete tasks, as well as manage tags that symbolize the theme of each task.
--- todo_list/settings.py
+++ todo_list/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-gnujsdyn2weio+c2fd3q-u)%9t=t6jdhuhn=6j@f0o3%$yqr_u"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
